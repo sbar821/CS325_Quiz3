9dc1f7634f7e36ae952928fddc3e4ce8772c06f6
Austin Morris <aumorri@siue.edu>
2024-01-28 23:03:06 -0600

    causing conflict

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ new_temp = (float(user_input) * 1.8) + 32
 print(f"The equivalent of {user_input} degrees Celsius is {new_temp:.1f} degrees Fahrenheit.")
 
 # change line #8 to cause conflict #
+print(f"Extra line to cause trouble :)")
